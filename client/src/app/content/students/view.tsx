78394c202096cc8b10a43b8f24360bada8ac7eec
gurochka <natali.ananyeva@gmail.com>
2020-07-07 14:59:40 +0400

    add materialUI

--- client/src/app/content/students/view.tsx
+++ client/src/app/content/students/view.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
 
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Box } from '@material-ui/core';
+
 export const View: React.FC = () => {
     return (
-        <div>
-            Students List
-        </div>
+        <Box width="50%" mx="auto" my={5}>
+            <TableContainer component={Paper}>
+                <Table aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Student name</TableCell>
+                            <TableCell align="center">Birthdate</TableCell>
+                            <TableCell align="right">Average assessment</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </Box>
     );
 };
